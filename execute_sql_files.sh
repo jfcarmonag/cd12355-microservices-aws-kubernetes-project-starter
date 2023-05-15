ae43728c7a0c215cf2f288528f360b248f27c500
Juan_Felipe <jfcarmonag@gmail.com>
2023-05-15 16:08:02 -0500

    update buildpsec

--- execute_sql_files.sh
+++ execute_sql_files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Retrieve the password
+export POSTGRES_PASSWORD=$(kubectl get secret --namespace default postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 -d)
+
 # First, set up port forwarding
 kubectl port-forward --namespace default svc/postgres-postgresql 5432:5432 &
 
@@ -9,7 +12,7 @@ sleep 5
 # Iterate over the SQL files in the db folder and execute each one
 for file in db/*.sql; do
     echo "Executing SQL file: $file"
-    PGPASSWORD="W1CSlGUN1Q" psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < "$file"
+    PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < "$file"
 done
 
 # Terminate the port forwarding process
